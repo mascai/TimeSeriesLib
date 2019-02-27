3e170f47f4b532ce18605c4de8939a76d36daa6c
mascai <msumoskalenko@gmail.com>
2019-02-28 00:22:05 +0300

    Add prettyPrint

--- TSL/main.cpp
+++ TSL/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -13,13 +14,19 @@ public:
     Series() : data_({}) {}
     Series(vector<T> data) : data_(data) {}
     Series(const Series& mSeries) : data_(mSeries.data) {}
-    //friend ostream& operator<<(ostream& os, const Series& series);
     friend ostream& operator<<(ostream& os, const Series<T>& series) {
         for (auto item : series.data_) {
             os << item << ' ';
         }
         os << '\n';
         return os;
+    }
+    void prettyPrint() {
+        // setw -- set width for one element
+        cout << fixed << setprecision(6);
+        for (auto i : data_) {
+            cout << setfill('#') << setw(5) << i;
+        }
     }
     T firstDiff(const int id);
 
@@ -37,8 +44,8 @@ T Series<T>::firstDiff(const int id) {
 
 int main()
 {
-    vector<int> vec{4, 5, 10, 8, 9};
-    Series<int> s1(vec);
-    cout << s1.firstDiff(2);
+    vector<double> vec{4.12345, 5.123, 10.1239, 8.12355, 9};
+    Series<double> s1(vec);
+    s1.prettyPrint();
     return 0;
 }
